b32438fe49bd5e3a82c990d26f728f4d4fa9ff03
Dao Hoang Son <daohoangson@gmail.com>
2025-12-31 15:37:08 +0700

    chore(release): v0.0.54-fork.1

--- cli.js
+++ cli.js
@@ -16,7 +16,7 @@
  */
 
 const { program } = require('playwright-core/lib/utilsBundle');
-const { decorateCommand } = require('playwright/lib/mcp/program');
+const { decorateCommand } = require('@scoutqa/playwright/lib/mcp/program');
 
 const packageJSON = require('./package.json');
 const p = program.version('Version ' + packageJSON.version).name('Playwright MCP');
